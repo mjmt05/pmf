051b53e75ca4b3d043f02039671c81733af4d5b6
Melissa Turcotte <mturcotte@octolabs.io>
2022-05-16 16:54:16 -0600

    pipenv

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@ setup(
     name='pmf',
     version='1.0.0',
     packages=['pmf', 'pmf.data', 'pmf.model', 'pmf.vi'],
-    install_requires=[
-    	'numpy==1.18.4',
-	    'tqdm==4.62.3',
-	    'scipy==1.4.1',
-	    'scikit_learn==1.0.2'
-    ],
     license='MIT'
 )
 
